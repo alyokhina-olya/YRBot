6b401eca947f78a8e25e83cb1588d558ba4e7f8b
alyokhina-o <alyokhina-o@yandex-team.ru>
2019-01-24 20:10:43 +0300

    added yandex-map-api call

--- index.js
+++ index.js
@@ -5,15 +5,14 @@ const functions = require('firebase-functions');
 const {WebhookClient} = require('dialogflow-fulfillment');
 const {Card, Suggestion} = require('dialogflow-fulfillment');
 const http = require('http'); 
+const https = require('https'); 
 const crypto = require('crypto');
+const yandexMapApiHost = 'search-maps.yandex.ru';
+const yandexMapApiKey = 'ad916bd9-1da4-470b-b83d-1db9007f61cf';
+const yandexGeoCodeApiHost = 'geocode-maps.yandex.ru';
+const yandexGeoCodeApiKey = '2ed59f1e-14d8-4c25-9811-9571046d08ff';
 
-const options = {
-      host: '35.227.52.98',
-      port: 8000,
-      path: '/test',
-      method: 'GET'
-    };
-    
+
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 
 const admin = require('firebase-admin');
@@ -26,6 +25,97 @@ function md5(string) {
     return crypto.createHash('md5').update(string).digest('hex');
 }
 
+function createYandexMapApiPath(text) {
+    return '/v1/?text=' + encodeURIComponent(text) + '&lang=ru_RU&apikey=' + yandexMapApiKey;
+}
+
+function createYandexGeoCodeApiPath(x, y) {
+    return '/1.x/?apikey='+yandexGeoCodeApiKey+ '&format=json&geocode=' + x + ',' + y;
+}
+
+function countYandexMapApiResponces(response) {
+    if (response === null 
+        || response === undefined 
+        || response.properties === null 
+        || response.properties === undefined
+        || response.properties.ResponseMetaData === null 
+        || response.properties.ResponseMetaData === undefined 
+        || response.properties.ResponseMetaData.SearchResponse === null 
+        || response.properties.ResponseMetaData.SearchResponse === undefined 
+        || response.properties.ResponseMetaData.SearchResponse.found === null
+        || response.properties.ResponseMetaData.SearchResponse.found === undefined) {
+            console.log("Incorrect json format\n expected *.properties.ResponseMetaData.SearchResponse.found,\n but found " + JSON.stringify(response));
+            return 0;
+        }
+    return response.properties.ResponseMetaData.SearchResponse.found;
+}
+
+function getYandexMapCoordinates(response) {
+    if (response === null 
+        || response === undefined 
+        || response.properties === null 
+        || response.properties === undefined
+        || response.properties.ResponseMetaData.SearchResponse === null 
+        || response.properties.ResponseMetaData.SearchResponse === undefined 
+        || response.properties.ResponseMetaData.SearchResponse.found === null
+        || response.properties.ResponseMetaData.SearchResponse.found === undefined
+        || response.properties.ResponseMetaData.SearchResponse.found === 0
+        || response.features === null
+        || response.features === undefined 
+        || response.features[0] === null
+        || response.features[0] === undefined 
+        || response.features[0].geometry === null
+        || response.features[0].geometry === undefined
+        || response.features[0].geometry.coordinates === null
+        || response.features[0].geometry.coordinates === undefined) {
+            return null;
+        }
+    return response.features[0].geometry.coordinates;
+}
+
+function getGeoObjectForDistrict(records) {
+    if (records === null || records === undefined) {
+        return null;
+    }
+    for (var i = 0; i < records.length; i++) {
+        if (records[i] !== null
+            && records[i] !== undefined
+            && records[i].GeoObject !== null
+            && records[i].GeoObject !== undefined
+            && records[i].GeoObject.metaDataProperty !== null
+            && records[i].GeoObject.metaDataProperty !== undefined
+            && records[i].GeoObject.metaDataProperty.GeocoderMetaData !== null
+            && records[i].GeoObject.metaDataProperty.GeocoderMetaData !== undefined
+            && records[i].GeoObject.metaDataProperty.GeocoderMetaData.kind !== null
+            && records[i].GeoObject.metaDataProperty.GeocoderMetaData.kind !== undefined
+            && records[i].GeoObject.metaDataProperty.GeocoderMetaData.kind === 'district'
+            && records[i].GeoObject.name !== null
+            && records[i].GeoObject.name !== null
+            && records[i].GeoObject.name.indexOf('район') !== -1) {
+            return records[i].GeoObject.name;
+        }
+    }
+    return null;
+}
+
+
+function getYandexGeoCodeDistrict(data) {
+    if (data !== null
+        && data !== undefined
+        &&  data.response !== null
+        &&  data.response !== undefined
+        &&  data.response.GeoObjectCollection !== null
+        &&  data.response.GeoObjectCollection !== undefined
+        && data.response.GeoObjectCollection.featureMember !== null
+        && data.response.GeoObjectCollection.featureMember !== undefined) {
+            let district = getGeoObjectForDistrict(data.response.GeoObjectCollection.featureMember);
+            district = district.replace(' район','');
+            return district;
+        }
+    return null;
+}
+
+
 function parseHttpResponse(output) {
     var offers = JSON.parse(output);
     if (offers.length === 0) {
@@ -36,6 +126,71 @@ function parseHttpResponse(output) {
         results = results + offers[i].url + "\n";
     }
     return results;
+}
+
+function callYandexGeoCodeApi(x, y) {
+    return new Promise((resolve, reject) => {
+        let path = createYandexGeoCodeApiPath(x, y);
+        console.log('API Request: ' + yandexGeoCodeApiHost + path);
+        https.get({host: yandexGeoCodeApiHost, path: path}, (res) => {
+            let body = '';
+            res.on('data', (d) => { body += d; });
+            res.on('end', () => {
+                console.log(JSON.stringify(body));
+                let response = JSON.parse(body);
+                let output = getYandexGeoCodeDistrict(response);
+                console.log('District: ' + output);
+                resolve(output);
+            });
+            res.on('error', (error) => {
+                console.log(`Error calling YandexGeoCodeApi: ${error}`);
+                reject();
+            });
+        });
+    });
+}
+
+function callYandexMapApiForAddress(text) {
+  return new Promise((resolve, reject) => {
+    let path = createYandexMapApiPath(text);
+    console.log('API Request: ' + yandexMapApiHost + path);
+
+    https.get({host: yandexMapApiHost, path: path}, (res) => {
+      let body = '';
+      res.on('data', (d) => { body += d; });
+      res.on('end', () => {
+        console.log(JSON.stringify(body));
+        let response = JSON.parse(body);
+        let output;
+        if (countYandexMapApiResponces(response) === 0) {
+            output = null;
+        }
+        else {
+            let coordinates = getYandexMapCoordinates(response);
+            if (coordinates === null 
+                || coordinates === undefined
+                || coordinates.length < 2) {
+                    output = null;
+                    resolve(output);
+                }
+            else {
+                callYandexGeoCodeApi(coordinates[0], coordinates[1]).then((res) => {
+                    output = res;
+                    console.log("Yandex map:" + text + " " + output);
+                    resolve(output);
+                }).catch(() => {
+                    output = null;
+                    resolve(output);
+                });
+            }
+        }
+      });
+      res.on('error', (error) => {
+        console.log(`Error calling YandexMapApiForAddress: ${error}`);
+        reject();
+      });
+    });
+  });
 }
 
 function defaultResponse(response, request) {
@@ -84,25 +239,11 @@ function max(array) {
 }
 
 
-function getPredResponse(sessionIdHash) {
-    return admin
-            .database()
-            .ref('sessionResponses')
-            .child(sessionIdHash)
-            .once('value')
-            .then(snapshot => {
-                const predResponse = snapshot.val();
-                console.log('filmsList: ' + JSON.stringify(predResponse));
-                return predResponse;
-            })
-            .catch(error => {
-                console.log('getPredResponse error: ' + error);
-                return error;
-            });
-}
 
-
-function addParams(bd, info, sessionIdHash) {
+function addParams(bd, info, sessionIdHash, district) {
+    
+    
+    
     if (info.lift !== undefined && info.lift !== null && info.lift !== "") {
         bd.sessionInfo[sessionIdHash].lift = true;
     }
@@ -132,8 +273,7 @@ function addParams(bd, info, sessionIdHash) {
     if (info.maxCountRooms !== undefined && info.maxCountRooms !== null && info.maxCountRooms !== "") {
         bd.sessionInfo[sessionIdHash].maxCountRooms  = info.maxCountRooms;
     }
-    
-    
+
     if (info.minCountRooms !== undefined && info.minCountRooms !== null && info.minCountRooms !== "") {
         bd.sessionInfo[sessionIdHash].minCountRooms  = info.minCountRooms;
     }
@@ -202,18 +342,48 @@ function addParams(bd, info, sessionIdHash) {
         bd.sessionInfo[sessionIdHash].metro = info.metro;
     }
     
-    if (info.district !== undefined &&info.district !== null &&info.district !== "") {
+    if (district !== undefined && district !== null && district !== "") {
+        bd.sessionInfo[sessionIdHash].district = [district];
+    }
+    
+    if (info.district !== undefined &&info.district !== null &&info.district !== ""
+       &&info.district.length !== 0) {
         bd.sessionInfo[sessionIdHash].district = info.district;
     }
+    
+    
     return bd;
 }
 
 
+function getDistrict(request) {
+    return new Promise((resolve, reject) => {
+        var sessionIdHash = md5(request.body.session);
+        if (request.body.queryResult.outputContexts.length >= 0 &&
+            request.body.queryResult.outputContexts[0].parameters !== null && 
+            request.body.queryResult.outputContexts[0].parameters !== undefined) {
+                let info = request.body.queryResult.outputContexts[0].parameters;
+                if (info.address !== undefined && info.address !== null && info.address !== "") {
+                    callYandexMapApiForAddress(info.address).then((district) => {
+                        resolve(district);
+                    }).catch((error) => {
+                        console.log('ERROR: ' + error);
+                        resolve(null);
+                    });
+                }
+                else {
+                    resolve(null);
+                }
+        }
+        else {
+            resolve(null);
+        }
+    });
+}
 
 
 
-
-function saveInfo(request, response) {
+function saveInfo(request, district) {
     admin.database().ref().transaction((session) => {
         if(session !== null && session.sessionInfo !== null) {
             var sessionIdHash = md5(request.body.session);
@@ -225,7 +395,7 @@ function saveInfo(request, response) {
                 request.body.queryResult.outputContexts[0].parameters !== undefined) {
                     session = addParams(session,
                                 request.body.queryResult.outputContexts[0].parameters,
-                                sessionIdHash);
+                                sessionIdHash, district);
                 }
         }
         return session;
@@ -233,6 +403,7 @@ function saveInfo(request, response) {
         console.log('Updation session Info with session = ' + request.body.session + ': '  + isSuccess);
     });
 }
+
 
 function callServer(sessionInfo) {
       return new Promise((resolve, reject) => {
@@ -297,17 +468,20 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
  
   
   
- 
- saveInfo(request);
-  if (request.body.queryResult.intent.displayName == 'Location Intent' ||
-      request.body.queryResult.intent.displayName == 'Improvement Intent') {
-      getApartment(request, response);
-  }
-  else if (request.body.queryResult.intent.displayName == 'Default Welcome Intent') {
+ getDistrict(request)
+ .then((district) => {
+    console.log("MAIN DISTRICT" + district);
+    saveInfo(request, district);
+    if (request.body.queryResult.intent.displayName == 'Location Intent' ||
+        request.body.queryResult.intent.displayName == 'Improvement Intent') {
+            getApartment(request, response);
+    }
+    else if (request.body.queryResult.intent.displayName == 'Default Welcome Intent') {
       clear(request);
       defaultResponse(response, request);
-  }
-  else {
-      defaultResponse(response, request);
-  }
+    }
+     else {
+        defaultResponse(response, request);
+    }
+    });
 });
